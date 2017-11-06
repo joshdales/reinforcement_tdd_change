a1eeb304f49926bc81be86fb142f07f412383359
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 17:30:30 -0500

    define method to make change

--- changer.rb
+++ changer.rb
@@ -1,11 +1,26 @@
 class Changer
 
   def initialize
-    @quarter = 25
-    @dime = 10
-    @five = 5
-    @penny = 1
   end
 
+  def self.make_change(num)
+    change = []
+    while change.sum < num
+      change << 25
+      if change.sum > num
+        change.pop
+      end
+      change << 10
+      if change.sum > num
+        change.pop
+      end
+      change << 5
+      if change.sum > num
+        change.pop
+      end
+      change << 1
+    end
+    return change
+  end
 
 end
