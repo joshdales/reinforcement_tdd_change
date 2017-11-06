22526411c5ad801b4d9728fdf8d8435b37643909
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 17:41:28 -0500

    Make adding change a method

--- changer.rb
+++ changer.rb
@@ -1,33 +1,61 @@
 class Changer
 
   def initialize
+    @change = []
   end
 
   def self.make_change(num)
-    change = []
 
     while change.sum < num
+      
       if change.sum == num
         return change.sort.reverse
       end
-      change << 25
+
+      @change.add_quarters
+
       if change.sum > num
         change.pop
       end
-      change << 10
+
+      @change.add_dimes
+
       if change.sum > num
         change.pop
       end
-      change << 5
+
+      @change.add_fives
+
       if change.sum > num
         change.pop
       end
-      change << 1
+
+      @change.add_pennies
+
       if change.sum > num
         change.pop
       end
+
     end
+
     return change.sort.reverse
+
+  end
+
+  def add_quarters
+    @change << 25
+  end
+
+  def add_dimes
+    @change << 10
+  end
+
+  def add_fives
+    @change << 5
+  end
+
+  def add_pennies
+    @change << 1
   end
 
 end
