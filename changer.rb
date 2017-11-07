bbada501675ec34f75e5c4abc23e5383f1ba81d3
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 19:42:55 -0500

    minus coins from change

--- changer.rb
+++ changer.rb
@@ -8,31 +8,33 @@ class Changer
       num = change / 25
       num.times do
         coins << 25
-        change - 25
+        change -= 25
       end
 
 
       num = change / 10
       num.times do
         coins << 10
-        change - 10
+        change -= 10
       end
 
 
       num = change / 5
       num.times do
         coins << 5
-        change - 5
+        change -= 5
       end
 
 
       num = change / 1
       num.times do
         coins << 1
-        change - 1
+        change -= 1
       end
 
       return coins.sort.reverse
     end
   end
+
+
 end
