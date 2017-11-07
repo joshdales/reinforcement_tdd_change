c69383a84d4af214d6a90b9314a9c90c1062df78
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 19:53:56 -0500

    DRY up code

--- changer.rb
+++ changer.rb
@@ -2,39 +2,18 @@ class Changer
 
   def self.make_change(change)
     coins = []
+    values = [25, 10, 5, 1]
 
     if change > 0
+      values.each do |value|
 
-      num = change / 25
+      num = change / value
       num.times do
-        coins << 25
-        change -= 25
+        coins << value
+        change -= value
+        end
       end
-
-
-      num = change / 10
-      num.times do
-        coins << 10
-        change -= 10
-      end
-
-
-      num = change / 5
-      num.times do
-        coins << 5
-        change -= 5
-      end
-
-
-      num = change / 1
-      num.times do
-        coins << 1
-        change -= 1
-      end
-
-      return coins.sort.reverse
+    return coins.sort.reverse
     end
   end
-
-
 end
